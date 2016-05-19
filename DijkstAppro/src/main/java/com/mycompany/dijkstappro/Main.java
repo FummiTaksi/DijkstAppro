c4c57d0ee93664e61ca156fa9756065f37fb0a3a
Aleksi <mustonea@aleksi-debian>
2016-05-19 13:20:09 +0300

    lisatty main

---
+++ DijkstAppro/src/main/java/com/mycompany/dijkstappro/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.dijkstappro;
+import java.util.*;
+
+/**
+ *
+ * @author mustonea
+ */
+public class Main {
+    public static void main(String[] arghh) {
+        System.out.println("Zaa jou");
+    }
+    
+}
