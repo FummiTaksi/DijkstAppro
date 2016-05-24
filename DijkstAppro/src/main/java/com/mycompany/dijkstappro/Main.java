6338bf2d4533f4484a1f1f541052afe1dbc4ebc5
Aleksi <mustonea@aleksi-debian>
2016-05-24 15:33:19 +0300

    koodausta jatkettu.

--- DijkstAppro/src/main/java/com/mycompany/dijkstappro/Main.java
+++ DijkstAppro/src/main/java/com/mycompany/dijkstappro/Main.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package com.mycompany.dijkstappro;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 /**
@@ -11,9 +14,38 @@ import java.util.*;
  * @author mustonea
  */
 public class Main {
-    
-    public static void main(String[] arghh) {
-        System.out.println("Zaa jou");
+     static int paikkojenLkm;
+     static Baari[] baarit;
+    public static void main(String[] arghh) throws FileNotFoundException {
+       paikkojenLkm = 0;
+       baarit =new Baari[100];
+       char[][] kartta = kartanAlustus("esim1.txt");          // muodostetaan tekstitiedostosta kartta taulukkoon
+       int aloitusIndeksi = 0;
+       while (aloitusIndeksi < paikkojenLkm) {
+           
+       }
+       
+        
     }
-    
+
+    public static char[][] kartanAlustus(String tiedostonNimi) throws FileNotFoundException {
+        char[][] kartta = new char[100][100];
+       
+        Scanner lukija = new Scanner(new File(tiedostonNimi), "UTF-8");
+        int riviNro = 0;
+        while (lukija.hasNextLine()) {
+            String rivi = lukija.nextLine();
+            for (int i = 0; i < rivi.length(); i++) {
+                char kirjain = rivi.charAt(i);
+                if (kirjain != '#' && kirjain != '.') {
+                    baarit[paikkojenLkm] = new Baari("" + kirjain,i,riviNro);
+                    paikkojenLkm++;
+                }
+                kartta[riviNro][i] = rivi.charAt(i);
+            }
+            riviNro++;
+        }
+        return kartta;
+    }
+
 }
